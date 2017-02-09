5ce0cb45898276d5be1f4915a06d334b7a6c87e3
Matheus <matheus.ps@icloud.com>
2017-02-09 20:04:54 -0300

    added angular CND

---
+++ js/app.js
@@ -0,0 +1,11 @@
+app = angular.module('starSky', []);
+
+app.controller('switchController', function ($scope) {
+    $scope.backStyle = "SUNSET";
+
+    $scope.changeBackground = function () {
+
+    };
+
+});
+
